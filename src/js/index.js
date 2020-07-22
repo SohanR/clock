3cac501d9fa009fecbf48b2d600fd1af9442208d
SohanR <mizan.rahman66d@gmail.com>
2020-07-22 22:12:21 +0600

    script

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const hourHand = document.querySelector(".hour-hand");
+const minHand = document.querySelector(".min-hand");
+const secHand = document.querySelector(".sec-hand");
+
+function setDate() {
+  const now = new Date();
+  const sec = now.getSeconds();
+
+  const secDegree = (sec / 60) * 360 + 90;
+  secHand.style.transform = `rotate(${secDegree}deg)`;
+
+  const min = now.getMinutes();
+  const minDegree = (min / 60) * 360 + (sec / 60) * 6 + 90;
+  minHand.style.transform = `rotate(${minDegree}deg)`;
+
+  const hour = now.getMinutes();
+  const hourDegree = (hour / 12) * 360 + (min / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
+}
+
+setInterval(setDate, 1000);
